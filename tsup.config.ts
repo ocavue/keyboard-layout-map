b6ffb44d3365d844207b1c654a87b67d7d9b5372
ocavue <ocavue@users.noreply.github.com>
2024-04-23 20:39:38 +1000

    refactor: move layouts to individual files (#6)

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/layouts.ts', 'src/types.ts'],
+  entry: {
+    index: 'src/index.ts',
+    layouts: 'src/layouts/index.ts',
+    types: 'src/types.ts',
+  },
   format: ['esm'],
   clean: true,
   dts: true,
